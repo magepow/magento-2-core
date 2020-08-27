670d046ca9457948a048ee34c4645b7ed690f21d
Alex Dong <support@alothemes.com>
2020-08-27 22:41:17 +0700

    Update grid-slider.js

--- view/base/web/js/grid-slider.js
+++ view/base/web/js/grid-slider.js
@@ -1,8 +1,8 @@
 /*
 * @Author: Alex Dong
 * @Date:   2020-07-29 13:21:07
-* @Last Modified by:   nguyen
-* @Last Modified time: 2020-08-18 20:28:33
+* @Last Modified by:   Alex Dong
+* @Last Modified time: 2020-08-27 22:40:56
 */
 
 define([
@@ -20,33 +20,44 @@ define([
                 this._initSlider();
             },
 
+			_uniqid: function (a = "", b = false) {
+			    const c = Date.now()/1000;
+			    let d = c.toString(16).split(".").join("");
+			    while(d.length < 14) d += "0";
+			    let e = "";
+			    if(b){
+			        e = ".";
+			        e += Math.round(Math.random()*100000000);
+			    }
+			    return a + d + e;
+			},
+
             _initSlider: function () {
                 var options = this.options;
                 var self = this;
                 self.element.find(options.selector).each(function() {
                     var el = $(this);
+                    var selector = 'grid-slider-' + self._uniqid();
+                    el.addClass(selector);
+                    selector = '.' + selector;
 		            if($('body').hasClass('rtl')){
 		                el.attr('dir', 'rtl');
 		                el.data( 'rtl', true );
-		                // el.data( 'infinite', false );
-		                // el.data( 'slides-to-scroll', -1 );
 		            }
 		            var options = el.data();
 		            if(iClass === undefined){
 		                el.children().addClass('alo-item');
 		                var iClass = '.alo-item';
 		            }
-		            var selector = el.selector;
-		            var classes = selector + ' '+ iClass;
-		            var padding = options.padding;
-		            var style = padding ? classes + '{float: left; padding-left: '+padding+'px; padding-right:'+padding+'px} ' + selector + '{margin-left: -'+padding+'px; margin-right: -'+padding+'px}' : '';
-		            $('head').append('<style type="text/css">'+style+'</style>');
+		            var classes	 = selector + ' '+ iClass;
+		            var padding  = options.padding;
+		            var float  	 = $('body').hasClass('rtl') ? 'right' : 'left';
+		            var style 	 = padding ? classes + '{float: ' + float + '; padding: 0 '+padding+'px; box-sizing: border-box} ' + selector + '{margin: 0 -'+padding+'px}' : '';
 		            if(options.slidesToShow){
 		                var lazy  = el.find('img.lazyload');
 		                if(lazy.length){
 		                    lazy.each(function(index) {
 		                        $(this).data('lazy', $(this).data('src'));
-		                        // $(this).attr('src', $(this).data('src')); // fixed data-adaptive-height="true"
 		                    });
 		                }
 		                el.on('init', function(event, slick){
@@ -70,15 +81,34 @@ define([
 		                    var video = $(this).find('.external-video');
 		                    video.removeClass('embed').find('img').show();
 		                    video.find('.iframe-video').remove();
-		                    // var slides = $(slick.$slides);
-		                    // slides.removeClass('abc-slick-active');
-		                    // var nxt_slider = $(slick.$slides.get(nextSlide));
-		                    // if(nxt_slider.length) nxt_slider.addClass('abc-slick-active');
 		                });
 		                slider.on( "click", ".item", function() {
 		                    el.slick('slickSetOption', "autoplay",false,false);
 		                }); 
+		            } else {
+						var responsive 	= options.responsive;
+						if(responsive == undefined) return;
+						var length = Object.keys(responsive).length;
+
+						$.each( responsive, function( key, value ) {
+							var col = 0;
+							var maxWith = 0;
+							var minWith = 0;
+							$.each( value , function(size, num) { minWith = parseInt(size) + 1; col = num;});
+							if(key+2<length){
+								$.each( responsive[key+1], function( size, num) { maxWith = size; col = num;});
+								style += ' @media (min-width: '+minWith+'px) and (max-width: '+maxWith+'px)';
+							} else { 
+								if(key+2 == length) return; // don't use key = length - 1;
+								$.each( responsive[key], function( size, num) { maxWith = size; col = num;});
+								style += ' @media (min-width: '+maxWith+'px)';
+							}
+							style += ' {'+selector + '{margin: 0 -'+padding+'px}'+classes+'{padding: 0 '+padding+'px; box-sizing: border-box; width: '+(Math.floor((10/col) * 100000000000) / 10000000000)+'%} '+classes+':nth-child('+col+'n+1){clear: ' + float + ';}}';
+						});		            	
 		            }
+
+		           	$('head').append('<style type="text/css">'+style+'</style>');
+
                 });
             }
         });
