c722af0e9c3298a4c152d68b91f4234a75a7007b
Alex Dong <team.magiccart@gmail.com>
2021-06-04 11:33:41 +0700

    Add jquery.easing.min.js

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -2,17 +2,22 @@ var config = {
 
 	map: {
 		'*': {
+			'easing': 'magepow/easing',
 			'slick': 'magepow/slick',
 			'gridSlider': 'magepow/grid-slider',
 		},
 	},
 
 	paths: {
+		'magepow/easing'		: 'Magepow_Core/js/plugin/jquery.easing.min',
 		'magepow/slick'			: 'Magepow_Core/js/plugin/slick.min',
 		'magepow/grid-slider'	: 'Magepow_Core/js/grid-slider',
 	},
 
 	shim: {
+		'magepow/easing': {
+			deps: ['jquery']
+		},
 		'magepow/slick': {
 			deps: ['jquery']
 		}
